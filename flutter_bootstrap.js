c2bdd0b48553bff9ac02fb8196cf5ec00109b422
Jusu Lamminheimo <jusulamminheimo@gmail.com>
2026-08-06 13:19:02 +0300

    1.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1775557422" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1289951827" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
